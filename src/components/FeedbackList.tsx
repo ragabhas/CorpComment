7b02af49ec189a0a7955efc7111fb022bd66a81d
Hassan.Ragab <Hassan.Ragab@thermofisher.com>
2024-06-19 17:47:01 +0200

    feat: Handle error when fetching feedbacks in FeedbackList

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,25 +1,37 @@
 import { useEffect, useState } from "react";
 import FeedbackItem, { FeedbackItemType } from "./FeedbackItem";
 import Spinner from "./Spinner";
+import ErrorMessage from "./ErrorMessage";
 
 export default function FeedbackList() {
   const [feedbackItems, setFeedbackItems] = useState<FeedbackItemType[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     setIsLoading(true);
     fetch(
       "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
-    ).then((response) => {
-      response.json().then((data) => {
-        setFeedbackItems(data.feedbacks);
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch feedbacks");
+        }
+        return response.json();
+      })
+      .then((data) => {
         setIsLoading(false);
+        setFeedbackItems(data.feedbacks);
+      })
+      .catch(() => {
+        setIsLoading(false);
+        setError("Failed to fetch feedbacks");
       });
-    });
   }, []);
 
   return (
     <ol className="feedback-list">
+      {error && <ErrorMessage message={error} />}
       {isLoading && <Spinner />}
       {feedbackItems &&
         feedbackItems.map((feedbackItem) => (
